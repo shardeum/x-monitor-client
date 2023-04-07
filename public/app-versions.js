ebef86495d899c8ad401040ca9118ab0bd16300f
Atharva <atharvakn@gmail.com>
2023-04-08 00:10:46 +0530

    Implement Pie chart that shows CLI/GUI & node ver

--- public/app-versions.js
+++ public/app-versions.js
@@ -1,29 +1,27 @@
 const INTERVAL = 10_000
 
 const fetchChanges = async () => {
-    const appVersionsResponse = await requestWithToken('/api/app-versions')
-    const appVersions = appVersionsResponse.data
-    const appVersionsWithColors = await Promise.all(
-        Object.keys(appVersions).map(async (appVersion) => {
-            const count = appVersions[appVersion]
-            const color = await stringToColour(appVersion)
+    const data = []
+    const labels = []
+    const tooltips = new Map()
 
-            return {
-                appVersion,
-                count,
-                color,
-            }
-        })
-    )
-    const totalNumNodes = appVersionsWithColors.reduce((acc, appVersionWithColor) => {
-        return acc + appVersionWithColor.count
-    }, 0)
+    const appDataResponse = await requestWithToken('/api/app-versions')
+    const appDataList = appDataResponse.data
 
-    drawPieChart(appVersionsWithColors, totalNumNodes)
+    for(const appVersion in appDataList) {
+        const count = appDataList[appVersion].nodeCount
+        data.push(count)
+        labels.push(appVersion)
+        const cliVersions = Object.entries(appDataList[appVersion].cliVersions).map(([version, count]) => `${version}: ${count}`).join("\n");
+        const guiVersions = Object.entries(appDataList[appVersion].guiVersions).map(([version, count]) => `${version}: ${count}`).join("\n");
+        tooltips.set(String(count), `cliVersions:\n${cliVersions}\nguiVersions:\n${guiVersions}`)
+    }
+
+    drawPieChart(data, labels, tooltips)
 }
 
 // From https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
-const stringToColour = async (str) => {
+const stringToColour = (str) => {
     var hash = 0
     for (var i = 0; i < str.length; i++) {
         hash = str.charCodeAt(i) + ((hash << 5) - hash)
@@ -31,47 +29,105 @@ const stringToColour = async (str) => {
     var color = '#'
     for (var i = 0; i < 3; i++) {
         var value = (hash >> (i * 8)) & 0xff
-        color += ('00' + value.toString(16)).substr(-2)
+        color += ('00' + value.toString(16)).substring(-2)
     }
     console.log('colour', color)
     return color
 }
 
-const drawPieChart = (appVersionsWithColors, totalNumNodes) => {
-    const canvas = document.getElementById('app-versions-chart')
-    const ctx = canvas.getContext('2d')
+const drawPieChart = (data, labels, tooltips) => {
+  const canvas = document.getElementById("app-versions-chart");
+  const ctx = canvas.getContext("2d");
 
-    let currentAngle = 0
-    const radius = 200
+  new Chart(ctx, {
+    type: "pie",
+    data: {
+    datasets: [{
+        data: data,
+    }],
+    labels: labels,
+    },
+    options: {
+        plugins: {
+            tooltip: {
+                enabled: false,
 
-    appVersionsWithColors.forEach((appVersionWithColor) => {
-        const { count, color, appVersion } = appVersionWithColor
-        const portionAngle = (count / totalNumNodes) * 2 * Math.PI
-        ctx.beginPath()
+                external: function(context) {
+                    // Tooltip Element
+                    let tooltipEl = document.getElementById('chartjs-tooltip');
 
-        ctx.arc(
-            canvas.width / 2,
-            canvas.height / 2,
-            radius,
-            currentAngle,
-            currentAngle + portionAngle
-        )
+                    // Create element on first render
+                    if (!tooltipEl) {
+                        tooltipEl = document.createElement('div');
+                        tooltipEl.id = 'chartjs-tooltip';
+                        tooltipEl.innerHTML = '<table></table>';
+                        document.body.appendChild(tooltipEl);
+                    }
 
-        ctx.lineTo(radius, radius)
+                    // Hide if no tooltip
+                    const tooltipModel = context.tooltip;
+                    if (tooltipModel.opacity === 0) {
+                        tooltipEl.style.opacity = 0;
+                        return;
+                    }
 
-        ctx.fillStyle = color
-        ctx.fill()
+                    // Set caret Position
+                    tooltipEl.classList.remove('above', 'below', 'no-transform');
+                    if (tooltipModel.yAlign) {
+                        tooltipEl.classList.add(tooltipModel.yAlign);
+                    } else {
+                        tooltipEl.classList.add('no-transform');
+                    }
 
-        const labelX = canvas.width / 2 + (radius / 2) * Math.cos(currentAngle + portionAngle / 2)
-        const labelY = canvas.height / 2 + (radius / 2) * Math.sin(currentAngle + portionAngle / 2)
-        const percentage = Math.round((100 * count) / totalNumNodes)
+                    function getBody(bodyItem) {
+                        return bodyItem.lines;
+                    }
 
-        ctx.fillStyle = 'white'
-        ctx.font = '20px Khand'
-        ctx.fillText(`v${appVersion} - ${percentage}%`, labelX, labelY)
+                    // Set Text
+                    if (tooltipModel.body) {
+                        const titleLines = tooltipModel.title || [];
+                        const bodyLines = tooltipModel.body.map(getBody);
 
-        currentAngle += portionAngle
-    })
+                        let innerHtml = '<thead>';
+
+                        titleLines.forEach(function(title) {
+                            innerHtml += '<tr><th>' + title + '</th></tr>';
+                        });
+                        innerHtml += '</thead><tbody>';
+
+                        bodyLines.forEach(function(body, i) {
+                            const colors = tooltipModel.labelColors[i];
+                            let style = 'background:' + colors.backgroundColor;
+                            style += '; border-color:' + colors.borderColor;
+                            style += '; border-width: 2px';
+                            const versions = tooltips.get(body[0])
+                            const html = versions.split("\n").map(line => `<p>${line}</p>`).join("");
+                            const span = '<span style="' + style + '">' + body + html + '</span>';
+                            innerHtml += '<tr><td>' + span + '</td></tr>';
+                            console.log(html)
+                        });
+                        innerHtml += '</tbody>';
+
+                        let tableRoot = tooltipEl.querySelector('table');
+                        tableRoot.innerHTML = innerHtml;
+                    }
+
+                    const position = context.chart.canvas.getBoundingClientRect();
+                    const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
+
+                    // Display, position, and set styles for font
+                    tooltipEl.style.opacity = 1;
+                    tooltipEl.style.position = 'absolute';
+                    tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+                    tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+                    tooltipEl.style.font = bodyFont.string;
+                    tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
+                    tooltipEl.style.pointerEvents = 'none';
+                }
+            }
+        }
+    }
+  });
 }
 
 fetchChanges()
