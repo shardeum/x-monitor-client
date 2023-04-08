e0bce829868ec476ba1702395713bcffc48377c8
Atharva <atharvakn@gmail.com>
2023-04-08 10:11:10 +0530

    Prettify pie chart tooltip

--- public/app-versions.js
+++ public/app-versions.js
@@ -3,7 +3,7 @@ const INTERVAL = 10_000
 const fetchChanges = async () => {
     const data = []
     const labels = []
-    const tooltips = new Map()
+    const tooltips = []
 
     const appDataResponse = await requestWithToken('/api/app-versions')
     const appDataList = appDataResponse.data
@@ -14,7 +14,7 @@ const fetchChanges = async () => {
         labels.push(appVersion)
         const cliVersions = Object.entries(appDataList[appVersion].cliVersions).map(([version, count]) => `${version}: ${count}`).join("\n");
         const guiVersions = Object.entries(appDataList[appVersion].guiVersions).map(([version, count]) => `${version}: ${count}`).join("\n");
-        tooltips.set(String(count), `cliVersions:\n${cliVersions}\nguiVersions:\n${guiVersions}`)
+        tooltips.push(`cliVersions:\n${cliVersions}\nguiVersions:\n${guiVersions}`)
     }
 
     drawPieChart(data, labels, tooltips)
@@ -38,6 +38,7 @@ const stringToColour = (str) => {
 const drawPieChart = (data, labels, tooltips) => {
   const canvas = document.getElementById("app-versions-chart");
   const ctx = canvas.getContext("2d");
+  console.log(tooltips)
 
   new Chart(ctx, {
     type: "pie",
@@ -50,80 +51,12 @@ const drawPieChart = (data, labels, tooltips) => {
     options: {
         plugins: {
             tooltip: {
-                enabled: false,
-
-                external: function(context) {
-                    // Tooltip Element
-                    let tooltipEl = document.getElementById('chartjs-tooltip');
-
-                    // Create element on first render
-                    if (!tooltipEl) {
-                        tooltipEl = document.createElement('div');
-                        tooltipEl.id = 'chartjs-tooltip';
-                        tooltipEl.innerHTML = '<table></table>';
-                        document.body.appendChild(tooltipEl);
+                callbacks: {
+                    afterBody: (context) => {
+                        return tooltips[context[0].dataIndex].split("\n")
                     }
-
-                    // Hide if no tooltip
-                    const tooltipModel = context.tooltip;
-                    if (tooltipModel.opacity === 0) {
-                        tooltipEl.style.opacity = 0;
-                        return;
-                    }
-
-                    // Set caret Position
-                    tooltipEl.classList.remove('above', 'below', 'no-transform');
-                    if (tooltipModel.yAlign) {
-                        tooltipEl.classList.add(tooltipModel.yAlign);
-                    } else {
-                        tooltipEl.classList.add('no-transform');
-                    }
-
-                    function getBody(bodyItem) {
-                        return bodyItem.lines;
-                    }
-
-                    // Set Text
-                    if (tooltipModel.body) {
-                        const titleLines = tooltipModel.title || [];
-                        const bodyLines = tooltipModel.body.map(getBody);
-
-                        let innerHtml = '<thead>';
-
-                        titleLines.forEach(function(title) {
-                            innerHtml += '<tr><th>' + title + '</th></tr>';
-                        });
-                        innerHtml += '</thead><tbody>';
-
-                        bodyLines.forEach(function(body, i) {
-                            const colors = tooltipModel.labelColors[i];
-                            let style = 'background:' + colors.backgroundColor;
-                            style += '; border-color:' + colors.borderColor;
-                            style += '; border-width: 2px';
-                            const versions = tooltips.get(body[0])
-                            const html = versions.split("\n").map(line => `<p>${line}</p>`).join("");
-                            const span = '<span style="' + style + '">' + body + html + '</span>';
-                            innerHtml += '<tr><td>' + span + '</td></tr>';
-                            console.log(html)
-                        });
-                        innerHtml += '</tbody>';
-
-                        let tableRoot = tooltipEl.querySelector('table');
-                        tableRoot.innerHTML = innerHtml;
-                    }
-
-                    const position = context.chart.canvas.getBoundingClientRect();
-                    const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
-
-                    // Display, position, and set styles for font
-                    tooltipEl.style.opacity = 1;
-                    tooltipEl.style.position = 'absolute';
-                    tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
-                    tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
-                    tooltipEl.style.font = bodyFont.string;
-                    tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
-                    tooltipEl.style.pointerEvents = 'none';
                 }
+
             }
         }
     }
