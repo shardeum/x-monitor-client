00897e7d0f99a6157a19ded4efdaf0ced0d09111
dnlbui <dnlbui@proton.me>
2024-07-19 18:58:32 -0500

    Add Node Loads page and functionality

---
+++ public/node-loads.js
@@ -0,0 +1,74 @@
+(function main() {
+  const G = {}
+  loadToken(G)
+  G.monitorServerUrl = monitorServerUrl || `https://127.0.0.1:3000/api`
+  G.REFRESH_TIME = 10000
+
+  new Vue({
+      el: '#app',
+      data() {
+          return {
+              nodeLoads: [],
+              sortKey: 'ip',
+              sortAsc: true,
+          }
+      },
+      computed: {
+          sortedNodes() {
+              return this.nodeLoads.sort((a, b) => {
+                  let modifier = this.sortAsc ? 1 : -1
+                  if (a[this.sortKey] < b[this.sortKey]) return -1 * modifier
+                  if (a[this.sortKey] > b[this.sortKey]) return 1 * modifier
+                  return 0
+              })
+          },
+      },
+      methods: {
+          sortTable(key) {
+              if (this.sortKey === key) {
+                  this.sortAsc = !this.sortAsc
+              } else {
+                  this.sortKey = key
+                  this.sortAsc = true
+              }
+          },
+          async fetchChanges() {
+              let res = await requestWithToken(
+                  `${G.monitorServerUrl}/report?timestamp=${G.lastUpdatedTimestamp}`
+              )
+              return res.data
+          },
+          updateNetworkStatus(report) {
+              this.nodeLoads = []
+              for (let nodeId in report.nodes.active) {
+                  const node = report.nodes.active[nodeId]
+                  this.nodeLoads.push({
+                      id: nodeId,
+                      ip: node.nodeIpInfo.externalIp,
+                      port: node.nodeIpInfo.externalPort,
+                      loadInternal: node.currentLoad.nodeLoad.internal,
+                      loadExternal: node.currentLoad.nodeLoad.external,
+                      queueLength: node.queueLength,
+                      queueTime: node.txTimeInQueue,
+                  })
+              }
+          },
+          async updateNodes() {
+              try {
+                  let changes = await this.fetchChanges()
+                  this.updateNetworkStatus(changes)
+              } catch (e) {
+                  console.log('Error while trying to update nodes.', e)
+              }
+          },
+          start() {
+              this.updateNodes()
+              setInterval(this.updateNodes, G.REFRESH_TIME)
+          },
+      },
+      mounted() {
+          console.log('Mounted')
+          this.start()
+      },
+  })
+})()
